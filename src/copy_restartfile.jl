d937a59bee3b0f2c32600668ebe0890c38c9ce32
Frederick Groth <fgroth@usm.lmu.de>
2025-11-10 18:27:06 +0100

    safety check before moving file, so we don't accidentally overwrite data. add functions to copy list of properties from hdf5 to new hdf5. split recursive copy function, extract sub-function for dataset type

--- src/copy_restartfile.jl
+++ src/copy_restartfile.jl
@@ -1,6 +1,46 @@
 using HDF5
 
 """
+    copy_restartfile_properties(cluster::String, method::String="";
+                                kwargs...)
+
+Copy all properties to new restartfile. See `copy_restartfile_properties_name`.
+"""
+function copy_restartfile_properties(cluster::String, method::String="";
+                                     kwargs...)
+    copy_restartfile_properties_name(restartfile_name(cluster, method);
+                                     kwargs...)
+end
+"""
+    copy_restartfile_properties_name(restartfile::String;
+                                     new_restartfile::String="", properties::Vector)
+
+Copy all properties to `new_restartfile`. If this is an empty String, use `restartfile*"_new.h5"` as new name.
+"""
+function copy_restartfile_properties_name(restartfile::String;
+                                          new_restartfile::String="", properties::Vector)
+    src = h5open(restartfile, "r")
+    if new_restartfile == ""
+        new_restartfile = restartfile[1:end-3]*"_new.h5"
+    end
+    dest = h5open(new_restartfile, "w")
+    for property in properties
+        src_obj = create_groups_as_necessary(src,property)
+        src_obj = src_obj[property[end]]
+        dest_obj = create_groups_as_necessary(dest, property, copy_from=src)
+        # make sure the last layer is prepared as well
+        if isa(src_obj, HDF5.Dataset)
+            # use some trick to make sure we always have the correct name, both for String and Vector{String} type property.
+            dest_obj = create_dataset(dest_obj, vcat(property,"")[end-1], typeof(read(src_onj)))
+        else
+            dest_obj = create_groups_as_necessary(dest_obj, vcat(property[end],""))
+        end
+
+        recursive_copy(src_obj, dest_obj)
+    end
+end
+
+"""
     cleanup_restartfile(cluster::String, method::String="")
 
 Remove all deleted entries and compactify the file.
@@ -21,13 +61,17 @@ function cleanup_restartfile_name(restartfile::String)
     dest = h5open(tmp_file, "w")
     try
         recursive_copy(src, dest)
+        copied_successfull=true
+    catch
+        copied_successfull=false
     finally
         close(src)
         close(dest)
     end
-    mv(tmp_file, restartfile, force=true)
+    if copied_successfull
+        mv(tmp_file, restartfile, force=true)
+    end
 end
-
 
 """
     recursive_copy(src::Union{HDF5.File,HDF5.Group}, dest::Union{HDF5.File,HDF5.Group})
@@ -47,13 +91,23 @@ function recursive_copy(src::Union{HDF5.File,HDF5.Group}, dest::Union{HDF5.File,
             new_group = create_group(dest, name)
             recursive_copy(obj, new_group)
         elseif isa(obj, HDF5.Dataset)
-            data = read(obj)
-            dest[name] = data
-            # Copy attributes of the dataset
-            for attr_name in keys(attributes(obj))
-                attr_value = read_attribute(obj, attr_name)
-                write_attribute(dest[name], attr_name, attr_value)
-            end
+            println(dest)
+            dest_obj = create_dataset(dest, name, datatype(obj), dataspace(obj))
+            recursive_copy(obj, dest_obj)
         end
+    end
+end
+"""
+    recursive_copy(src::HDF5.Dataset, dest::HDF5.Dataset)
+
+Copy over the dataset from `src` to `dest`
+"""
+function recursive_copy(src::HDF5.Dataset, dest::HDF5.Dataset)
+    data = read(src)
+    write(dest, data)
+    # Copy attributes of the dataset
+    for attr_name in keys(attributes(src))
+        attr_value = read_attribute(src, attr_name)
+        write_attribute(dest, attr_name, attr_value)
     end
 end
